84ba3c8945c5b939704d4ddfadd0611ca15e95a1
Adrian <adrian2009@live.com.mx>
2021-11-29 22:14:04 -0700

    Updates

---
+++ index.js
@@ -0,0 +1,135 @@
+import * as THREE from './js/build/three.module.js';
+import {GLTFLoader} from './js/examples/jsm/loaders/GLTFLoader.js'
+import { VRButton } from './js/examples/jsm/webxr/VRButton.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.xr.enabled = true;
+container.appendChild( renderer.domElement );
+
+const onWindowResize = ()=>{
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight );
+}
+
+window.addEventListener( 'resize', onWindowResize );
+
+
+document.body.appendChild( VRButton.createButton( renderer ) );
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//const cube = new THREE.Mesh( geometry, material );
+//scene.add( cube );
+
+const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+scene.add( light );
+
+const loader = new GLTFLoader();
+
+
+loader.load( './models/Eduardo_4.gltf', 
+ ( gltf ) => {
+
+  scene.add( gltf.scene );
+},
+
+// called while loading is progressing
+( xhr ) => {
+
+  console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+},
+// called when loading has errors
+( error ) => {
+
+  console.log( 'An error happened' );
+
+});
+
+loader.load( './models/casa_4.gltf', 
+ ( gltf ) => {
+
+  scene.add( gltf.scene );
+},
+
+// called while loading is progressing
+( xhr ) => {
+
+  console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+},
+// called when loading has errors
+( error ) => {
+
+  console.log( 'An error happened' );
+
+});
+
+loader.load( './models/jarron.gltf', 
+ ( gltf ) => {
+
+  scene.add( gltf.scene );
+},
+
+// called while loading is progressing
+( xhr ) => {
+
+  console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+},
+// called when loading has errors
+( error ) => {
+
+  console.log( 'An error happened' );
+
+});
+
+loader.load( './models/mesa.gltf', 
+ ( gltf ) => {
+
+  scene.add( gltf.scene );
+},
+
+// called while loading is progressing
+( xhr ) => {
+
+  console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+},
+// called when loading has errors
+( error ) => {
+
+  console.log( 'An error happened' );
+
+});
+
+/*camera.position.z = 300;
+camera.position.y = 300;
+camera.position.x = 1;*/
+
+function animate() {
+
+  renderer.setAnimationLoop( ()=>{
+    renderer.render( scene, camera );
+  });
+
+}
+animate();
