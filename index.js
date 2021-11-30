be600bf3210d13601d904bcc3143bdd40af82468
Adrian <adrian2009@live.com.mx>
2021-11-30 00:20:05 -0700

    Updates

--- index.js
+++ index.js
@@ -103,9 +103,9 @@ loader.load( './models/mesa.gltf',
 
 });
 
-camera.position.z = 300;
-camera.position.y = 300;
-camera.position.x = 1;
+//camera.position.z = 300;
+//camera.position.y = 300;
+//camera.position.x = 1;
 
 function onWindowResize() {
 
